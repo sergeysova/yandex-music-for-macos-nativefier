34997ba135c97d570af08fd9557cfc0d14defea7
Sergey Sova <mail@sergeysova.com>
2021-01-22 14:36:44 +0300

    build without docker

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 ![Яндекс.Музыка для macOS](./app-screenshot.jpg)
 
-Создает SSB-приложение Яндекс.Музыки c помощью 
+Создает SSB-приложение Яндекс.Музыки c помощью
 [jiahaog/nativefier](https://github.com/jiahaog/nativefier).
 
 Поддерживается темная тема сайта: _(Аватар) => Настройки => Прочее => Темная тема_
 
-Для генерации приложения необходим [Docker Desktop](https://www.docker.com/products/docker-desktop).
+Для генерации приложения необходимо установить nativefier и Nodejs:
+
+
+Например, с помощью [brew](https://brew.sh):
+```shell
+brew install nvm
+
+# Node Version Manager
+nvm install 14
+
+# Node Package Manager
+npm install --global nativefier
+```
--- build.sh
+++ build.sh
@@ -2,26 +2,23 @@
 
 echo "`awk -F '=' '/BUILD_VERSION/{$2=$2+1}1' OFS='=' .build-config`"  > .build-config
 source .build-config
-USER_AGENT="YaMusic for macOS Version ${APP_VERSION} (build ${BUILD_VERSION} / Nativefier ${NATIVEFIER_VERSION}) https://github.com/ofstudio/yandex-music-for-macos-nativefier"
+USER_AGENT="Yandex.Music for macOS Version ${APP_VERSION} (build ${BUILD_VERSION} / Nativefier ${NATIVEFIER_VERSION})"
 echo "Building ${USER_AGENT}"
 
 rm -rf ./build
+mkdir ./build
 
-docker run  \
-  --rm \
-  -v `pwd`/build:/build \
-  -v `pwd`/src:/src \
-  jiahaog/nativefier \
+nativefier \
   -p mac \
+  --verbose \
   --app-version "${APP_VERSION}" \
   --build-version "${BUILD_VERSION}" \
-  --app-copyright "${APP_COPYRIGHT}" \
   --user-agent "${USER_AGENT}" \
   --browserwindow-options '{ "fullscreenable": "true", "titleBarStyle": "hiddenInset", "transparent": true }' \
-  --icon /src/icon.icns \
-  --inject /src/inject.js \
-  --inject /src/inject.css \
+  --icon ./src/icon.icns \
+  --inject ./src/inject.js \
+  --inject ./src/inject.css \
   --fast-quit \
   --internal-urls 'music\.yandex\.ru' \
-  --name YaMusic \
-  https://music.yandex.ru/ /build
+  --name Yandex.Music \
+  https://music.yandex.ru/ ./build
